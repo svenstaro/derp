89b970a5c75e47bf31c777ec895d47638974a021
Paul Bienkowski <opatutlol@aol.com>
2012-04-01 04:02:34 +0200

    Added main callbacks and a main loop to app and test functions in derper

--- derp/app.d
+++ derp/app.d
@@ -3,7 +3,59 @@ module derp.app;
 import std.stdio;
 
 class Derp {
+    /// Is being set to false when the main loop should end.
+    bool running = false;
+
+    void function() loadCallback;
+    void function() runCallback;
+    void function() drawCallback;
+    void function(float) updateCallback;
+    void function() quitCallback;
+
     this() {
         writeln("Derpy is coming!");
+    }
+
+    void run() {
+        if(loadCallback) {
+            loadCallback();
+        }
+
+        if(runCallback) {
+            runCallback();
+        } else {
+            mainLoop();
+        }
+
+        if(quitCallback) {
+            quitCallback();
+        }
+    }
+
+    void quit() {
+        running = false;
+    }
+
+private:
+    void mainLoop() {
+        running = true;
+        while(running) {
+            // Time keeping here
+            float delta_time = 0.5;
+
+            // Update everything
+            if(updateCallback) {
+                updateCallback(delta_time);
+            }
+
+            // Clear here automatically ?
+
+            // DRAW
+            if(drawCallback) {
+                drawCallback();
+            }
+
+            // Flip here automatically ?
+        }
     }
 }
--- derper/main.d
+++ derper/main.d
@@ -1,7 +1,32 @@
+import std.stdio;
+import std.conv;
+
 import derp.app;
 
-int main() {
-    Derp app = new Derp();
+float total_time = 0;
+Derp app;
 
+void load() {
+    writeln("Loading...");
+}
+
+void draw() {
+    writeln("Drawing...");
+}
+
+void update(float dt) {
+    total_time += dt;
+    writeln("Updating... " ~ to!string(dt));
+    if(total_time > 4) {
+        app.quit();
+    }
+}
+
+int main() {
+    app = new Derp();
+    app.drawCallback = &draw;
+    app.loadCallback = &load;
+    app.updateCallback = &update;
+    app.run();
     return 0;
 }
