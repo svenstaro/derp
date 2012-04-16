d2694b42fca4c2efe55ba2734d09ff7aa7fa8e0e
Zoadian <github@zoadian.de>
2012-04-16 19:29:28 +0000

    started to implement camera component. added various math stuff to geo.d. improved Angle in geo.d

--- derp/core/geo.d
+++ derp/core/geo.d
@@ -7,6 +7,7 @@ module derp.core.geo;
 
 public import gl3n.linalg; // let everyone access vec2 etc.
 import std.math;
+import std.traits;
 
 alias vec2 Vector2;
 alias vec3 Vector3;
@@ -20,46 +21,196 @@ alias mat34 Matrix34;
 alias mat4 Matrix4;
 alias quat Quaternion;
 
+@property Vector3 xAxis(in Quaternion q) @safe nothrow {
+    real ty = 2.0f*q.y;
+    real tz = 2.0f*q.z;
+    real twy = ty*q.w;
+    real twz = tz*q.w;
+    real txy = ty*q.x;
+    real txz = tz*q.x;
+    real tyy = ty*q.y;
+    real tzz = tz*q.z;
+    return Vector3(1.0f-(tyy+tzz), txy+twz, txz-twy);
+}
+
+@property Vector3 yAxis(in Quaternion q) @safe nothrow {
+    real tx = 2.0f*q.x;
+    real ty = 2.0f*q.y;
+    real tz = 2.0f*q.z;
+    real twx = tx*q.w;
+    real twz = tz*q.w;
+    real txx = tx*q.x;
+    real txy = ty*q.x;
+    real tyz = tz*q.y;
+    real tzz = tz*q.z;
+    return Vector3(txy-twz, 1.0f-(txx-tzz), tyz+twx);
+}
+
+@property Vector3 zAxis(in Quaternion q) @safe nothrow {
+    real tx = 2.0f*q.x;
+    real ty = 2.0f*q.y;
+    real tz = 2.0f*q.z;
+    real twx = ty*q.w;
+    real twy = tz*q.w;
+    real txx = ty*q.x;
+    real txz = tz*q.x;
+    real tyy = ty*q.y;
+    real tyz = tz*q.z;
+    return Vector3(txz+twy, tyz-twx, 1.0f-(txx+tyy));
+}
+
+void fromAngleAxis(ref Quaternion q, in Angle angle, in Vector3 axis) @safe nothrow {
+    float degree = angle.degrees;
+    degree /= 2;
+    auto s = sin(degree);
+    q.w = cos(degree);
+    q.x = s*axis.x;
+    q.y = s*axis.y;
+    q.z = s*axis.z;
+}
+
+auto ref makeTransform(ref Matrix4 matrix, in Vector3 position, in Vector3 scale, in Quaternion orientation) @safe nothrow {
+    // Ordering:
+    //    1. Scale
+    //    2. Rotate
+    //    3. Translate
+
+    Matrix3 rot3x3 = orientation.to_matrix!(3,3);
+
+    // Set up final matrix with scale, rotation and translation
+    matrix[0][0] = scale.x * rot3x3[0][0]; 
+    matrix[0][1] = scale.x * rot3x3[0][1];
+    matrix[0][2] = scale.x * rot3x3[0][2];
+    matrix[1][0] = scale.y * rot3x3[1][0]; 
+    matrix[1][1] = scale.y * rot3x3[1][1]; 
+    matrix[1][2] = scale.y * rot3x3[1][2]; 
+    matrix[2][0] = scale.z * rot3x3[2][0]; 
+    matrix[2][1] = scale.z * rot3x3[2][1]; 
+    matrix[2][2] = scale.z * rot3x3[2][2]; 
+    matrix[3][0] = position.x; 
+    matrix[3][1] = position.y; 
+    matrix[3][2] = position.z;
+
+    // No projection term
+    matrix[0][3] = 0; 
+    matrix[1][3] = 0; 
+    matrix[2][3] = 0; 
+    matrix[3][3] = 1;
+    return matrix;
+}
+
+
+
+Quaternion rotationTo(in Vector3 src, in Vector3 dest, in Vector3 fallbackAxis = Vector3(0,0,0)) @safe nothrow
+{
+	// Based on Stan Melax's article in Game Programming Gems
+	Quaternion q;
+	// Copy, since cannot modify local
+	Vector3 v0 = src;
+	Vector3 v1 = dest;
+	v0.normalize();
+	v1.normalize();
+
+	real d = dot(v0, v1);
+	// If dot == 1, vectors are the same
+	if (d >= 1.0f)
+	{
+		return Quaternion(0,0,0,1);
+	}
+	if (d < (1e-6f - 1.0f))
+	{
+		if (fallbackAxis != Vector3(0,0,0))
+		{
+			// rotate 180 degrees about the fallback axis
+			fromAngleAxis(q, degrees(180), fallbackAxis);
+		}
+		else
+		{
+			// Generate an axis
+			enum VX = Vector3(1,0,0);
+			Vector3 axis = cross(VX, src);
+			if (axis.length == 0) // pick another if colinear
+				axis = cross(Vector3(0,1,0), src);
+			axis.normalize();
+			fromAngleAxis(q, degrees(180), axis);
+		}
+	}
+	else
+	{
+		real s = sqrt( (1+d)*2 );
+		real invs = 1 / s;
+
+		Vector3 c = cross(v0, v1);
+
+		q.x = c.x * invs;
+		q.y = c.y * invs;
+		q.z = c.z * invs;
+		q.w = s * 0.5f;
+		q.normalize();
+	}
+	return q;
+}
+
+
+
+
+
 struct Angle {
 private:
     float _asRadians;
 public:
-    @property float radians() {
+    @property float radians() pure const @safe nothrow {
         return this._asRadians;
     }
 
-    @property float radians(float radians) {
+    @property float radians(float radians) pure @safe nothrow {
         this._asRadians = radians;
         return this._asRadians;
     }
 
-    @property float degrees() {
+    @property float degrees() pure const @safe nothrow {
         return this._asRadians * 180.0 / PI;
     }
 
-    @property float degrees(float degrees) {
+    @property float degrees(float degrees) pure @safe nothrow {
         this._asRadians = degrees * PI / 180.0;
         return degrees;
     }
 
-    Angle opBinary(string op, T)(T inp) const if((op == "*") || (op == "/")) {
-        Angle ret;
-        mixin("ret.radians = this.radians " ~ op ~ " inp.radians;");
+    /// opBinary
+    Angle opBinary(string op, T)(T inp) pure const @safe nothrow {
+        Angle ret = this;
+        mixin("ret "~op~"= inp;");
         return ret;
     }
+    
+    /// opOpAssign a numeric type
+    auto ref opOpAssign(string op, T)(T inp) pure @safe nothrow
+    if(isNumeric!T &&((op == "*") || (op == "/"))) {
+        mixin ("this._asRadians "~op~"= inp;");
+        return this;
+    }
+    
+    /// opOpAssign an Angle
+    auto ref opOpAssign(string op)(Angle inp) pure @safe nothrow
+    if(op == "*" || op == "/") {
+        mixin ("this._asRadians "~op~"= inp.radians;");
+        return this;
+    }
 
-    Angle opBinaryRight(string op, T)(T inp) const if((op == "*") || (op == "/")) {
+    Angle opBinaryRight(string op, T)(T inp) pure const @safe nothrow
+    if((op == "*") || (op == "/")) {
         return this.opBinary!(op)(inp);
     }
 }
 
-Angle radians(float radians) {
+Angle radians(float radians) pure @safe nothrow {
     Angle a;
     a.radians = radians;
     return a;
 }
 
-Angle degrees(float degrees) {
+Angle degrees(float degrees) pure @safe nothrow {
     Angle a;
     a.degrees = degrees;
     return a;
--- derp/core/scene.d
+++ derp/core/scene.d
@@ -5,6 +5,7 @@
 module derp.core.scene;
 
 import std.algorithm;
+import std.math;
 
 import derp.core.geo;
 
@@ -47,36 +48,36 @@ private:
     bool _needDerivedMatrixUpdate = true;
 
 public:
-    this(string name, Node parent = null) {
+    this(string name, Node parent = null) @safe nothrow {
         this._name = name;
         setParent(parent);
         this.scale = Vector3(1, 1, 1);
     }
     
     /// Returns this Nodes name.
-    @property string name() const {
+    @property string name() const @safe nothrow {
         return this._name;
     }
 
     /// Position relative to parent.
-    @property Vector3 position() const {
+    @property Vector3 position() const @safe nothrow {
         return this._localPosition;
     }
 
     /// ditto
-    @property Vector3 position(in Vector3 position) {
+    @property Vector3 position(in Vector3 position) @safe nothrow {
         this._requestUpdate(Update.Position);
         this._localPosition = position;
         return this._localPosition;
     }
 
     /// Orientation relative to parent.
-    @property Quaternion orientation() const {
+    @property Quaternion orientation() const @safe nothrow {
         return this._localOrientation;
     }
 
     /// ditto
-    @property Quaternion orientation(Quaternion orientation) {
+    @property Quaternion orientation(Quaternion orientation) @safe nothrow {
         // Note: normalisation is not _required_ here
         orientation.normalize();
         this._requestUpdate(Update.Orientation);
@@ -85,19 +86,19 @@ public:
     }
 
     /// Scale relative to parent.
-    @property Vector3 scale() const {
+    @property Vector3 scale() const @safe nothrow {
         return this._localScale;
     }
 
     /// ditto
-    @property Vector3 scale(in Vector3 scale) {
+    @property Vector3 scale(in Vector3 scale) @safe nothrow {
         this._requestUpdate(Update.Scale);
         this._localScale = scale;
         return this._localScale;
     }
 
     /// Position relative to world.
-    @property Vector3 derivedPosition() {
+    @property Vector3 derivedPosition() @safe nothrow {
         if(this._needPositionUpdate && this._parent) {
             // Change position vector based on parent's orientation & scale
             this._cachedDerivedPosition = this._parent.derivedOrientation * Vector3(
@@ -114,7 +115,7 @@ public:
     }
 
     /// Orientation relative to world.
-    @property Quaternion derivedOrientation() {
+    @property Quaternion derivedOrientation() @safe nothrow {
         if(this._needOrientationUpdate && this._parent !is null)
             this._cachedDerivedOrientation = this.orientation * this._parent.derivedOrientation;
         else if(this._needOrientationUpdate && this._parent is null)
@@ -124,7 +125,7 @@ public:
     }
 
     /// Scale relative to world.
-    @property Vector3 derivedScale() {
+    @property Vector3 derivedScale() @safe nothrow {
         if(this._needScaleUpdate && this._parent !is null) {
             this._cachedDerivedScale = Vector3(
                 this.scale.x * this._parent.derivedScale.x,
@@ -138,7 +139,7 @@ public:
     }
 
     /// Local transformation Matrix.
-    @property Matrix4 matrix() {
+    @property Matrix4 matrix() @safe nothrow {
         if(this._needMatrixUpdate) {
             this._cachedMatrix = this.orientation.to_matrix!(4, 4).translate(this.position.x, this.position.y, this.position.z).scale(this.scale.x, this.scale.y, this.scale.z);
             this._needMatrixUpdate = false;
@@ -147,7 +148,7 @@ public:
     }
 
     /// Global transformation Matrix.
-    @property Matrix4 derivedMatrix() {
+    @property Matrix4 derivedMatrix() @safe nothrow {
         if(this._needDerivedMatrixUpdate) {
             if(this._parent !is null) {
                 this._cachedDerivedMatrix = this._parent.derivedMatrix * this._cachedMatrix;
@@ -160,7 +161,7 @@ public:
     }
 
     /// Translates this Node by a Vector.
-    void translate(in Vector3 vector, TransformSpace ts = TransformSpace.Local) {
+    void translate(in Vector3 vector, TransformSpace ts = TransformSpace.Local) @safe nothrow {
         if(ts == TransformSpace.Local) {
             this.position += this.orientation * vector;
         } else if(ts == TransformSpace.Parent) {
@@ -180,12 +181,12 @@ public:
     }
 
     /// Rotates this Node by the Angle around the given axis.
-    void rotate(Angle angle, Vector3 axis, TransformSpace ts) {
+    void rotate(Angle angle, Vector3 axis, TransformSpace ts) @safe nothrow {
         this.rotate(Quaternion.axis_rotation(angle.radians, axis), ts);
     }
 
     /// Rotates this Node by Quaternion
-    void rotate(Quaternion rotation, TransformSpace ts) {
+    void rotate(Quaternion rotation, TransformSpace ts) @safe nothrow {
         rotation.normalize();
 
         if(ts == TransformSpace.Local) {
@@ -200,14 +201,17 @@ public:
 
 public:
     /// Creates a new Node with this Node as parent.
-    Node createChildNode(string name) {
+    Node createChildNode(string name) @safe nothrow {
         return new Node(name, this);
     }
 
     /// Attaches this Node to a parent Node.
-    void setParent(Node parent) {
+    /// TODO: @safe, remove trycatch from chountuntil
+    void setParent(Node parent) @trusted nothrow {
         if(this._parent !is null) {
-            remove(this._parent._children, countUntil(this._parent._children, this));
+            try {
+                remove(this._parent._children, countUntil(this._parent._children, this));
+            }catch(Exception e){}
         }
         if(parent !is null) {
             parent._children ~= this;
@@ -217,19 +221,19 @@ public:
 
     /// Returns a path for user-friendly representation of the position
     /// of this Node in the scene graph (separated by '/' slashes).
-    @property string path() const {
+    @property string path() const @safe nothrow {
         if(this._parent) return this._parent.path ~ "/" ~ this._name;
         else return this._name;
     }
 
     /// Returns whether this Node is a root Node (has no parent).
-    @property bool isRoot() const {
+    @property bool isRoot() const @safe nothrow {
         return (this._parent is null);
     }
 
     /// Returns the root node of this Node (the first Node in the scene
     /// graph).
-    @property Node rootNode() {
+    @property Node rootNode() @safe nothrow {
         Node n = this;
         while(!n.isRoot)
             n = n._parent;
@@ -237,12 +241,17 @@ public:
     }
     
     /// Returns true if node is a child of this Node.
-    bool hasChildNode(Node node) const {
-        return countUntil(this._children, node) != -1;
+    /// TODO: @safe, remove try catch block from countuntil
+    bool hasChildNode(Node node) const @trusted nothrow {
+        try{
+            return countUntil(this._children, node) != -1;
+        }catch(Exception e){
+            return false;
+        }
     }
     
     /// Returns true if node is a sibling of this Node
-    bool hasSiblingNode(Node node) const {
+    bool hasSiblingNode(Node node) const @safe nothrow {
         foreach(e; this._children)
             if(e is node || e.hasSiblingNode(node))
                 return true;
@@ -250,19 +259,19 @@ public:
     }
     
     /// Returns count of children this Node has.
-    ulong countChildren() const {
+    ulong countChildren() const @safe nothrow {
         return this._children.length;
     }
     
     /// Returns array of children this Node has.
-    @property const(Node[]) children() const {
+    @property const(Node[]) children() const @safe nothrow {
         return this._children;
     }
     
     /// Returns the Node identified by name if it is a 
     /// sibling of this Node.
     /// If it is not found it returns null.
-    Node findSibling(string name) {
+    Node findSibling(string name) @safe nothrow {
         foreach(e; this._children)
             if(e._name == name) {
                 return e;
@@ -277,18 +286,21 @@ public:
     
 public:
     /// Attach a Component to this Node.
-    void attachComponent(Component c) {
+    void attachComponent(Component c) @safe nothrow {
         c._node = this;
     }
     
     /// Detach a Component from this Node
-    void detachComponent(Component c) {
+    /// TODO: @safe, remove try catch block from countUntil
+    void detachComponent(Component c) @trusted nothrow {
         c._node = null;
-        remove(this._components, countUntil(this._components, c));
+        try {
+            remove(this._components, countUntil(this._components, c));
+        }catch(Exception e){}
     }
     
     // Returns all components attached to this node
-    @property const(Component[]) components() const {
+    @property const(Component[]) components() const @safe nothrow {
         return this._components;
     }        
 
@@ -301,7 +313,7 @@ private:
 
     /// Informs the Node and all child nodes to update parts of their
     /// cached transformation state.
-    void _requestUpdate(Update update) {
+    void _requestUpdate(Update update) @safe nothrow {
         if(update == Update.Position)
             this._needPositionUpdate = true;
         if(update == Update.Scale)
@@ -327,24 +339,195 @@ private:
     string _name;
 
 public:
-    this(string name) {
+    this(string name) @safe nothrow {
         this._name = name;
     }
 
     /// Returns a path for user-friendly representation of the position
     /// of this Component in the scene graph (separated by '/' slashes).
-    @property string path() const {
+    @property string path() const @safe nothrow {
         if(this._node) return this._node.path ~ "/" ~ this._name;
         else return "<unattached>/" ~ this._name;
     }
     
     /// Returns the Node this Component is attached to.
-    @property Node node() {
+    @property Node node() @safe nothrow {
         return this._node;
     }
     
     /// Returns this Components name.
-    @property string name() const {
+    @property string name() const @safe nothrow {
         return this._name;
     }
+}
+
+/**
+ * CameraComponent
+ */
+class CameraComponent : Component{
+private:	
+    Matrix4 _projectionMatrix;
+    Matrix4 _cachedViewMatrix;
+    Vector3 _position;
+    Quaternion	_orientation;
+    bool _needUpdate = true;
+public:
+    ///
+    this(string name, Angle fovY, float aspectRatio, float front, float back) @safe nothrow {
+        float tangent = tan((fovY/2).radians);   // tangent of half fovY
+        float height = front * tangent;          // half height of near plane
+        float width = height * aspectRatio;      // half width of near plane
+        // params: left, right, bottom, top, near, far
+        this(name, -width, width, -height, height, front, back);
+    }	
+
+    /**
+        uniform perspective projection matrix,
+        // which depth range [-1,1], right-handed rules
+        //
+        // [ A   0   C   0  ]
+        // [ 0   B   D   0  ]
+        // [ 0   0   q   qn ]
+        // [ 0   0   -1  0  ]
+        //
+    */
+    this(string name, float left, float right, float top, float bottom, float near, float far) @safe nothrow {		
+        super(name);
+        float A = 2 * near / (right - left);
+        float B = 2 * near / (top - bottom);
+        float C = (right + left) / (right - left);
+        float D = (top + bottom) / (top - bottom);
+        float q = - (far + near) / (far - near);
+        float qn = - 2 * (far * near) / (far - near);
+        this._projectionMatrix = Matrix4(Vector4(A,0,0,0),Vector4(0,B,0,0),Vector4(C,D,q,-1),Vector4(0,0,qn,0));
+    }
+    
+public:
+    ///
+    @property Vector3 position() const @safe nothrow {
+        return this._position;
+    }
+
+    ///
+    @property Vector3 position(in Vector3 position) @safe nothrow {
+        this._needUpdate = true; 
+        this._position = position; 
+        return this._position;
+    }
+
+    ///
+    @property Quaternion orientation() const @safe nothrow {
+        return this._orientation;
+    } 
+
+    ///
+    @property Quaternion orientation(in Quaternion quaternion) @safe nothrow {
+        this._needUpdate = true; 
+        this._orientation = orientation; 
+        return this._orientation;
+    }
+
+    ///return projection matrix
+    @property Matrix4 projectionMatrix() const @safe nothrow {
+        return _projectionMatrix;
+    }
+
+    /// return cached view matrix. if nessacary, generate it
+    @property Matrix4 viewMatrix() @safe nothrow {
+        if(this._needUpdate)
+        {
+            makeTransform(this._cachedViewMatrix, this._position, Vector3(1,1,1), this._orientation);
+            this._needUpdate = false;
+        }
+        return this._cachedViewMatrix;
+    }
+    
+public:
+    ///translate (TransformSpace.Parent == TransformSpace.World)
+    void translate(in Vector3 vector, TransformSpace ts) @safe nothrow {
+        this._needUpdate = true; 
+        final switch(ts)
+        {
+        case TransformSpace.Local:
+            this._position += this._orientation * vector;
+            break;
+        case TransformSpace.Parent:
+            this._position += vector;
+            break;
+        case TransformSpace.World:
+            this._position += vector;
+            break;
+        }
+    }
+
+    ///rotate (TransformSpace.Parent == TransformSpace.World)
+    void rotate(in Angle angle, in Vector3 axis, TransformSpace ts) @safe nothrow {
+        Quaternion q;
+        final switch(ts)
+        {
+            case TransformSpace.Local:
+                fromAngleAxis(q, angle, this._orientation * axis);
+                break;
+            case TransformSpace.Parent:
+                fromAngleAxis(q, angle, axis);
+                break;
+            case TransformSpace.World:
+                fromAngleAxis(q, angle, axis);
+                break;
+        }
+        rotate(q);
+    }
+
+    ///rotate
+    void rotate(Quaternion quaternion) @safe nothrow {
+        this._needUpdate = true;
+        // Normalise the quat to avoid cumulative problems with precision
+        quaternion.normalize();
+        this._orientation = quaternion * this._orientation;
+    }
+
+    ///
+    @property void direction(Vector3 vector) @safe nothrow {
+        this._needUpdate = true;
+        if(vector == Vector3(0,0,0))
+            return;
+
+        Vector3 zAdjustVec = -vector;
+        zAdjustVec.normalize();
+
+        Quaternion targetWorldOrientation;
+        Quaternion rotQuat;
+        if ( (zAxis(this.orientation)+zAdjustVec).length_squared <  0.00005f) 
+        {
+            // Oops, a 180 degree turn (infinite possible rotation axes)
+            // Default to yaw i.e. use current UP
+            fromAngleAxis(rotQuat, degrees(180), yAxis(this.orientation));
+        }
+        else
+        {
+            // Derive shortest arc to new direction
+            rotQuat = rotationTo(zAxis(this.orientation), zAdjustVec);
+        }
+        this.orientation = rotQuat * this.orientation;
+    }
+
+    ///
+    void lookAt(Vector3 point) @safe nothrow {
+        this.direction = (point - this.position);
+    }	
+    
+public:
+    /// Returns a Ray From ScreenPoint into the Scene
+    //~ Ray cameraToViewportRay(uint screenX, uint screenY) {
+        //~ Matrix4 inverseVP = this.projectionMatrix * this.viewMatrix.inverse();
+        //~ real nx = (2.0f * screenX) - 1.0f;
+        //~ real ny = 1.0f - (2.0f * screenY);
+        //~ Vector3 nearPoint = Vector3(nx, ny, -1.0f);
+        //~ Vector3 midPoint = Vector3(nx, ny, 0.0f);
+        //~ Ray ray;
+        //~ ray.origin = inverseVP * nearPoint;
+        //~ ray.direction = (inverseVP * midPoint) - ray.origin;
+        //~ ray.direction.normalize();
+        //~ return ray;
+    //~ }
 }
