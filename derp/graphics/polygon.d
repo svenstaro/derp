65b8b1cf47cb50724df033d439e085f105ff70ef
Paul Bienkowski <opatutlol@aol.com>
2012-09-08 03:02:52 +0200

    Fixed compilation error

--- derp/graphics/polygon.d
+++ derp/graphics/polygon.d
@@ -79,7 +79,7 @@ public:
     }
     
     void prepareRender(RenderQueue queue) {
-        queue.push(this);
+        queue.push(cast(Renderable)this);
     }
 
     void render(RenderQueue queue) {
